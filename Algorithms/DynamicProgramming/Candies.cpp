213554aa34328866fd10307e7059228707574377
Humachine <swarun@gmail.com>
2016-05-08 15:07:25 -0700

    Began Candy Dyanmic Programming Problem

---
+++ Algorithms/DynamicProgramming/Candies.cpp
@@ -0,0 +1,18 @@
+//https://www.hackerrank.com/challenges/candies
+//
+
+#include<algorithm>
+#include<iostream>
+using namespace std;
+int minCandies(int *arr, int N);
+int main()
+{
+    int ratings[10000];
+    int N;
+    cin>>N;
+    int i, j, k, temp;
+    for(i=0;i<N;i++){
+        cin>>ratings[i];
+    }
+    return 0;
+}
